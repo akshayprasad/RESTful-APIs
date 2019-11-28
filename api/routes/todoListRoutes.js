3f2b85757688d861dc50dceca9d00921c2615c84
Akshay Prasad <akshpras@cisco.com>
2019-11-28 11:26:19 +0530

    [28-11-2019] Added update and delete methods to match as per CRUD opertions.

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -12,4 +12,10 @@ module.exports = function(app) {
     .get(todoList.read_a_task)
     .put(todoList.update_a_task)
     .delete(todoList.delete_a_task);
+
+    app.route('/tasks/delete/:taskId')
+    .delete(todoList.delete_a_task);
+
+    app.route('/tasks/update/:taskId')
+    .put(todoList.update_a_task);
 };
